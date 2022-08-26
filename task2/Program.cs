85d2fbb9df99b18a6a34b7e4ebd2f20eef80c585
Dmitriy B <kupperfeet@gmail.com>
2022-08-27 01:28:07 +0300

    binar

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу преобразования десятичного числа в двоичное
+Console.Write("Введите число: ");
+int tenNumber = int.Parse(Console.ReadLine() ?? "0");
+int binar = 0;
+int i = 0;
+while (tenNumber >= 1) 
+{
+    binar = binar + tenNumber%2*(int)Math.Pow(10,i);
+    tenNumber = tenNumber/2;
+    i++;
+}
+Console.WriteLine(binar);

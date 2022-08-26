730388e1d6b81c7a5a6f68d97a2300a36535dd39
Dmitriy B <kupperfeet@gmail.com>
2022-08-27 00:28:35 +0300

    bool beggin

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Выяснить являются ли три числа сторонами треугольника
+
+bool CheckTriangle(int a, int b, int c) => a+b>c && b+c>a && a+c>b;
+
+
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine() ?? "0");
+
+bool check = CheckTriangle(number1, number2, number3);
+Console.WriteLine(check ? "Три числа являются сторонами треугольника" :
+"Три числа не являются сторонами треугольника");
